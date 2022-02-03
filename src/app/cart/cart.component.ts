7f40b9b34a8b7956761bda0b73d611f960651aca
Gokhan-Unal <unal_gokhan@outlook.com>
2022-02-03 15:33:30 +0300

    form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
 
-  items = this.cartService.getItems()
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
 
-  constructor(private cartService: CartService) { }
-
-  ngOnInit(): void {
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your Order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
 
+  items = this.cartService.getItems();
+  ngOnInit(): void {}
 }
